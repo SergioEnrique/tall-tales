c7e185d7116232a5307cb04b60734746156534c9
Sergio Enrique Vargas <sergioenrique@me.com>
2017-04-05 05:11:30 +0200

    ya se selecciona

--- controllers/firstLesson.js
+++ controllers/firstLesson.js
@@ -12,15 +12,30 @@ angular.module('talesApp.firstLesson', ['ui.router'])
 function (                       $scope,   $http) {
 
 	var words = []
+	var blancoSelected = ''
+
 	$scope.selectedWords = []
+	$scope.blanco = {
+		a: '',
+		b: '',
+		c: '',
+		d: '',
+		e: '',
+		f: ''
+	}
 
 	$http.get('data/words.json')
 	.then(function (res) {
 		words = res.data
 	})
 
-	$scope.selectType = function(type) {
+	$scope.selectType = function(blanco, type) {
+		blancoSelected = blanco
 		$scope.selectedWords = words.find(x => x.type === type).words
+	}
+
+	$scope.selectWord = function(word) {
+		$scope.blanco[blancoSelected] = word
 	}
 
 }])
