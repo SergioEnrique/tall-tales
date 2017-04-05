6306d3610a8efa540f63725bcae86599dea5a3d0
Sergio Enrique Vargas <sergioenrique@me.com>
2017-04-05 04:05:42 +0200

    finalizado

--- controllers/firstLesson.js
+++ controllers/firstLesson.js
@@ -12,10 +12,15 @@ angular.module('talesApp.firstLesson', ['ui.router'])
 function (                       $scope,   $http) {
 
 	var words = []
+	$scope.selectedWords = []
 
 	$http.get('data/words.json')
 	.then(function (res) {
 		words = res.data
 	})
+
+	$scope.selectType = function(type) {
+		$scope.selectedWords = words.find(x => x.type === type).words
+	}
 
 }])
