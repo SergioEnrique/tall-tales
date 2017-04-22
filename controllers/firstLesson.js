cbf13cdf98093cf1b80022aaa1f30f970b67c9ba
Sergio Enrique Vargas <sergioenrique@me.com>
2017-04-22 19:27:12 +0200

    Segunda versión con diseño

--- controllers/firstLesson.js
+++ controllers/firstLesson.js
@@ -11,31 +11,4 @@ angular.module('talesApp.firstLesson', ['ui.router'])
 .controller('firstLessonCtrl', ['$scope', '$http',
 function (                       $scope,   $http) {
 
-	var words = []
-	var blancoSelected = ''
-
-	$scope.selectedWords = []
-	$scope.blanco = {
-		a: '',
-		b: '',
-		c: '',
-		d: '',
-		e: '',
-		f: ''
-	}
-
-	$http.get('data/words.json')
-	.then(function (res) {
-		words = res.data
-	})
-
-	$scope.selectType = function(blanco, type) {
-		blancoSelected = blanco
-		$scope.selectedWords = words.find(x => x.type === type).words
-	}
-
-	$scope.selectWord = function(word) {
-		$scope.blanco[blancoSelected] = word
-	}
-
 }])
--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,33 @@
 angular.module('talesApp.main', [])
 
-.controller('mainCtrl', ['$scope',
-function (                $scope) {
+.controller('mainCtrl', ['$scope', '$http',
+function (                $scope,   $http) {
+
+	var words = []
+	var blancoSelected = ''
+
+	$scope.selectedWords = []
+	$scope.blanco = {
+		a: '',
+		b: '',
+		c: '',
+		d: '',
+		e: '',
+		f: ''
+	}
+
+	$http.get('data/words.json')
+	.then(function (res) {
+		words = res.data
+	})
+
+	$scope.selectType = function(blanco, type) {
+		blancoSelected = blanco
+		$scope.selectedWords = words.find(x => x.type === type).words
+	}
+
+	$scope.selectWord = function(word) {
+		$scope.blanco[blancoSelected] = word
+	}
 
 }])
